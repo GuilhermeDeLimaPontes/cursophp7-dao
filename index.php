4ac95413947bab9005d35afbcfb7cb9a000e6130
GuilhermeDeLimaPontes <guilhermepontes6@gmail.com>
2019-02-07 17:57:51 -0200

    metodos de lista

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <?php 
 require_once("config.php");
 
-$guilherme = new Usuario();
+//carrega um usuario
+//$guilherme = new Usuario();
+//$guilherme->loadById(4);
+//echo $guilherme;
 
-$guilherme->loadById(4);
+//carega uma lista de usuario
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $guilherme;
+//carrega uma lista de usuarios buscando pelo login
+//$lista = Usuario::search("t");
+//echo json_encode($lista);
+
+//carrega um usuario usando o login e senha
+
+$usuario = new Usuario();
+
+$usuario->login("thamiris","negona");
+
+echo $usuario;
+
 
 ?>
